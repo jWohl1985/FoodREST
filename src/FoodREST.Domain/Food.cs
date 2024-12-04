bf07faa405c83013e1c551ef48d85fc081aad736
Jeremy Wohl <jdwohl@gmail.com>
2024-12-03 20:06:39 -0800

    Food entity and tests

---
+++ src/FoodREST.Domain/Food.cs
@@ -0,0 +1,32 @@
+﻿using Ardalis.GuardClauses;
+
+namespace FoodREST.Domain;
+
+public sealed class Food
+{
+    public Food(string name, 
+        int calories = 0, 
+        int proteinGrams = 0, 
+        int carbohydrateGrams = 0, 
+        int fatGrams = 0)
+    {
+        Id = Guid.NewGuid();
+        Name = Guard.Against.NullOrEmpty(name);
+        Calories = Guard.Against.Negative(calories);
+        ProteinGrams = Guard.Against.Negative(proteinGrams);
+        CarbohydrateGrams = Guard.Against.Negative(carbohydrateGrams);
+        FatGrams = Guard.Against.Negative(fatGrams);
+    }
+
+    public Guid Id { get; private set; }
+
+    public string Name { get; private set; }
+
+    public int Calories { get; private set; }
+
+    public int ProteinGrams { get; private set; }
+
+    public int CarbohydrateGrams { get; private set; }
+
+    public int FatGrams { get; private set; }
+}
---
+++ test/FoodREST.Domain.Tests.Unit/FoodTests.cs
@@ -0,0 +1,107 @@
+﻿using FluentAssertions;
+using System.Reflection;
+
+namespace FoodREST.Domain.Tests.Unit;
+
+public class FoodTests
+{
+    [Fact]
+    public void Id_ShouldNotBeEmpty()
+    {
+        // Arrange
+        var food = new Food(name: "Banana");
+
+        // Act
+        var result = food.Id;
+
+        // Assert
+        result.Should().NotBeEmpty();
+    }
+
+    [Fact]
+    public void Name_ShouldNotBeNull()
+    {
+        // Arrange
+        var food = () => new Food(name: null!);
+
+        // Act
+
+        // Assert
+        food.Should().ThrowExactly<ArgumentNullException>();
+    }
+
+    [Fact]
+    public void Name_CannotBeEmpty()
+    {
+        // Arrange
+        var food = () => new Food(name: "");
+
+        // Act
+
+        // Assert
+        food.Should().ThrowExactly<ArgumentException>();
+    }
+
+    [Fact]
+    public void Calories_ShouldNotBeNegative()
+    {
+        // Arrange
+        var food = () => new Food(name: "Banana", calories: -15);
+
+        // Act
+
+        // Assert
+        food.Should().ThrowExactly<ArgumentException>();
+    }
+
+    [Fact]
+    public void ProteinGrams_ShouldNotBeNegative()
+    {
+        // Arrange
+        var food = () => new Food(name: "Banana", proteinGrams: -1);
+
+        // Act
+
+        // Assert
+        food.Should().ThrowExactly<ArgumentException>();
+    }
+
+    [Fact]
+    public void CarbohydrateGrams_ShouldNotBeNegative()
+    {
+        // Arrange
+        var food = () => new Food(name: "Banana", carbohydrateGrams: -1);
+
+        // Act
+
+        // Assert
+        food.Should().ThrowExactly<ArgumentException>();
+    }
+
+    [Fact]
+    public void FatGrams_ShouldNotBeNegative()
+    {
+        // Arrange
+        var food = () => new Food(name: "Banana", fatGrams: -1);
+
+        // Act
+
+        // Assert
+        food.Should().ThrowExactly<ArgumentException>();
+    }
+
+    [Fact]
+    public void NutritionInfo_ShouldDefaultToZero_WhenNotSupplied()
+    {
+        // Arrange
+        var food = new Food(name: "Banana");
+
+        // Act
+
+        // Assert
+        food.Calories.Should().Be(0);
+        food.ProteinGrams.Should().Be(0);
+        food.CarbohydrateGrams.Should().Be(0);
+        food.FatGrams.Should().Be(0);
+    }
+}
